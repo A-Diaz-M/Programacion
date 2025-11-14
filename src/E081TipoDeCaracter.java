1ae8ebab671057c13c9287f4f668a1d547c3c4e4
A-Diaz-M <alejandro.diaz36@educa.madrid.org>
2025-11-14 12:08:21 +0100

    a

--- src/E081TipoDeCaracter.java
+++ src/E081TipoDeCaracter.java
@@ -1,5 +1,4 @@
 String caracter (char caracter) {
-
     return switch (caracter) {
         case 'a', 'b', 'c', 'd', 'e', 'f',
              'g', 'h', 'i', 'j', 'k', 'l',
@@ -10,23 +9,27 @@ String caracter (char caracter) {
              'G', 'H', 'I', 'J', 'K', 'L',
              'M', 'N', 'Ñ', 'O', 'P', 'Q',
              'R', 'S', 'T', 'U', 'V', 'W',
-             'X', 'Y', 'Z'                 -> "letra mayuscula";
-        case 1, 2, 3, 4, 5, 6, 7, 8, 9, 0  -> "numero";
+             'X', 'Y', 'Z'                 -> "LETRA MAYUSCULA";
+        case '1', '2', '3', '4', '5', '6',
+             '7', '8', '9', '0'            -> "numero";
         case ',', '.', '-', '(', ')', '¡',
-             '!', '¿', '?', ' '                 -> "símbolo";
-        default -> throw new IllegalStateException("Unexpected value: " + caracter);
+             '!', '¿', '?', ';', ' '            -> "símbolo";
+        default                            -> throw new IllegalStateException("Unexpected value: " + caracter);
     };
-
 }
 
-
-
 void main() {
+    Scanner sc = new Scanner(System.in, "UTF-8");
+
     IO.println("DÍAS DE UN MES");
     IO.println("Introduzca un numero y se dira cuantos dias tiene el mes.\n");
-    String nombre = IO.readln("Dime como te llamas:\n").toUpperCase();
 
-    String frase = IO.readln(nombre + ", introduzca una frase:\n");
+    System.out.println("Dime como te llamas:");
+    String nombre = sc.nextLine().toUpperCase();
+
+    System.out.println(nombre + ", introduzca una frase:");
+    String frase = sc.nextLine();
+
     for (int i = 0; i < frase.length() ; i++) {
         char letra = frase.charAt(i);
         IO.println(caracter(letra));
