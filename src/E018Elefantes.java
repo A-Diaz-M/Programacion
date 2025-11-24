e0cd3009c290e96cd4dc3ddca048db31cca6c7b9
A-Diaz-M <alejandro.diaz36@educa.madrid.org>
2025-11-24 17:32:13 +0100

    a

--- src/E018Elefantes.java
+++ src/E018Elefantes.java
@@ -4,20 +4,14 @@ void main() {
 
     int num = Integer.parseInt(IO.readln(nombre+", introduzca un numero:\n"));
 
-    String frase1;
-    String frase2;
-
     for (int i = 1; i <= num; i++){
-        frase1 = "elefante";
-        frase2 = "balanceaba";
-        if (i > 1){
-            frase1 = "elefantes";
-            frase2 = "balanceaban";
-        }
-        if (i > num * 0.75){
-            frase1 = "ELEFANTES";
-            frase2 = "BALANCEABAN";
-        }
-        System.out.format("Había una vez %d %s que se %s en una hamaca a la orilla de un río...\n", i, frase1, frase2);
+        boolean singular = (i == 1);
+
+        String elefante = singular ? "elefante" : "elefantes";
+        String balanceaba = singular ? "balanceaba" : "balanceaban";
+
+        if (i >= num * 0.75) elefante = elefante.toUpperCase();
+
+        IO.println(String.format("Había una vez %d %s que se %s en una hamaca a la orilla de un río...", i, elefante, balanceaba));
     }
 }
