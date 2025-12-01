3f2d01c8650131cbbcddf8c90743bc0129d39416
A-Diaz-M <alejandro.diaz36@educa.madrid.org>
2025-12-01 14:00:03 +0100

    Programas

--- src/b100arrays/E106MayorYMenor.java
+++ src/b100arrays/E106MayorYMenor.java
@@ -4,14 +4,17 @@ void main() {
     int num = Integer.parseInt(IO.readln(nombre + ", cuantos numeros quieres?\n"));
     int[] nums = new int[num];
 
-    int numero = 0;
+    int numeroMayor = Integer.MIN_VALUE;
+    int numeroMenor = Integer.MAX_VALUE;
 
     for (int i = 0; i < nums.length; i++) {
         nums[i] = Integer.parseInt(IO.readln(nombre + ", introduzca el " + (i + 1) + " numero.\n"));
-        if (nums[i] > numero) {
-            numero = nums[i];
+        if (nums[i] > numeroMayor) {
+            numeroMayor = nums[i];
+        }
+        if (nums[i] < numeroMenor) {
+            numeroMenor = nums[i];
         }
     }
-
-    IO.println(String.format("%s, el numero mayor es %d", nombre, numero));
+    IO.println(String.format("%s, el numero mayor es %d y el menor es %d", nombre, numeroMayor, numeroMenor));
 }
---
+++ src/b999random/imprimirArray.java
@@ -0,0 +1,5 @@
+void main() {
+    int[] array = {23, 45 ,99};
+
+    IO.println(Arrays.toString(array));
+}
