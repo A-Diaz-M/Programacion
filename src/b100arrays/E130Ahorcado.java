2cc1e3df91774def71743aba6f01f8799767caff
A-Diaz-M <alejandro.diaz36@educa.madrid.org>
2025-12-03 13:59:53 +0100

    Programas

---
+++ src/b100arrays/E130Ahorcado.java
@@ -0,0 +1,141 @@
+void main() {
+    String nombre = IO.readln("Dime como te llamas:\n").toUpperCase();
+
+    int cantLetras = Integer.parseInt(IO.readln(nombre + ", introduzca la cantidad de letras que tiene tu palabra:\n"));
+
+    char[] array = new char[cantLetras];
+    boolean[] aciertos = new boolean[cantLetras];
+
+    for (int i = 0; i < array.length; i++) {
+        array[i] = IO.readln(nombre + ", introduzca una letra:\n").charAt(0);
+    }
+
+    IO.println("\n--- JUGADOR 2 ---\n");
+
+    String nombre2 = IO.readln("Dime como te llamas:\n").toUpperCase();
+    int intentos = 0;
+    boolean todasAcertadas;
+    IO.println(String.format("Tienes %d intentos.", intentos));
+
+    do {
+        IO.print("Progreso: ");
+        for (int i = 0; i < array.length; i++) {
+            if (aciertos[i]) {
+                IO.print(array[i] + " ");
+            } else {
+                IO.print("_ ");
+            }
+            IO.println("");
+
+            System.out.println(" ____");
+
+            switch (intentos) {
+                case 1 -> {
+                    System.out.println(" |  0");
+                    System.out.println(" |");
+                    System.out.println(" |");
+                }
+                case 2 -> {
+                    System.out.println(" |  0");
+                    System.out.println(" |  |");
+                    System.out.println(" |");
+                }
+                case 3 -> {
+                    System.out.println(" |  0");
+                    System.out.println(" | -|");
+                    System.out.println(" |");
+                }
+                case 4 -> {
+                    System.out.println(" |  0");
+                    System.out.println(" | -|-");
+                    System.out.println(" |");
+                }
+                case 5 -> {
+                    System.out.println(" |  0");
+                    System.out.println(" | -|-");
+                    System.out.println(" | /");
+                }
+                case 6 -> {
+                    System.out.println(" |  0");
+                    System.out.println(" | -|-");
+                    System.out.println(" | / \\ ");
+                }
+            }
+            System.out.println("_|_");
+            System.out.println();
+        }
+
+        char letraComprobada = IO.readln(nombre2 + ", introduce una letra:\n").charAt(0);
+
+        boolean encontrada = false;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == letraComprobada) {
+                aciertos[i] = true;
+                encontrada = true;
+            }
+        }
+
+        if (encontrada) {
+            IO.println("LETRA ENCONTRADA.");
+        } else {
+            IO.println("Letra no encontrada.");
+            intentos++;
+            IO.println("Intentos: " + intentos);
+        }
+
+        todasAcertadas = true;
+        for (boolean acierto : aciertos) {
+            if (!acierto) {
+                todasAcertadas = false;
+                break;
+            }
+        }
+
+    } while (intentos > 0 && !todasAcertadas);
+
+    if (intentos == 6) {
+        IO.println("GAME OVER");
+    } else {
+        IO.println("\n¡Muy bien! Has adivinado todas las letras:");
+        for (char c : array) {
+            IO.print(c + " ");
+        }
+        System.out.println(" ____");
+
+        switch (intentos) {
+            case 1 -> {
+                System.out.println(" |  0");
+                System.out.println(" |");
+                System.out.println(" |");
+            }
+            case 2 -> {
+                System.out.println(" |  0");
+                System.out.println(" |  |");
+                System.out.println(" |");
+            }
+            case 3 -> {
+                System.out.println(" |  0");
+                System.out.println(" | -|");
+                System.out.println(" |");
+            }
+            case 4 -> {
+                System.out.println(" |  0");
+                System.out.println(" | -|-");
+                System.out.println(" |");
+            }
+            case 5 -> {
+                System.out.println(" |  0");
+                System.out.println(" | -|-");
+                System.out.println(" | /");
+            }
+            case 6 -> {
+                System.out.println(" |  0");
+                System.out.println(" | -|-");
+                System.out.println(" | / \\ ");
+            }
+        }
+        System.out.println("_|_");
+        System.out.println();
+    }
+}
