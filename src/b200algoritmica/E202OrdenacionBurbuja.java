50891ae5275a71a132367a76e5ea5ad022de27e5
A-Diaz-M <alejandro.diaz36@educa.madrid.org>
2025-12-04 14:05:39 +0100

    Programas

--- src/b200algoritmica/E202OrdenacionBurbuja.java
+++ src/b200algoritmica/E202OrdenacionBurbuja.java
@@ -3,7 +3,7 @@ void main() {
     int numGuardado;
 
     for (int i = 0; i < lista.length - 1; i++) {
-        for (int j = 0; j < lista.length - 1; j++) {
+        for (int j = 0; j < lista.length - 1 - i; j++) {
             if (lista[j] > lista[j + 1]) {
                 numGuardado = lista[j + 1];
                 lista[j + 1] = lista[j];
