ecf1c6277ce799bb7203fbf0324181e9ec9b93be
A-Diaz-M <alejandro.diaz36@educa.madrid.org>
2026-01-09 20:10:42 +0100

    a

--- src/b200algoritmica/E209Calculadora.java
+++ src/b200algoritmica/E209Calculadora.java
@@ -3,9 +3,9 @@ void main(String[] args) {
 
     int validacion;
     do {
-        double num = Integer.parseInt(IO.readln("Introduzca un numero:\n"));
+        double num = Double.parseDouble(IO.readln("Introduzca un numero:\n"));
         int funcion = Integer.parseInt(IO.readln("Introduce \n '1' para sumar              '+'\n '2' para restar             '-'\n '3' para multiplicar        '*'\n '4' para dividir            '/'\n '5' para hacer potencia     '^'\n '6' para hacer Raíz n-ésima '√'\n"));
-        double num2 = Integer.parseInt(IO.readln("Introduzca un numero:\n"));
+        double num2 = Double.parseDouble(IO.readln("Introduzca un numero:\n"));
 
         double resultado = 0;
 
@@ -17,9 +17,7 @@ void main(String[] args) {
             case 5 -> resultado = Math.pow(num, num2);
             case 6 -> resultado = Math.pow(num, 1.0/num2);
         }
-
-        IO.println(String.format("El resultado es: %.2f", resultado));
+        IO.println(String.format("El resultado es: %.4f", resultado));
         validacion = Integer.parseInt(IO.readln("Si no desea seguir usando la calculadora introduzca '0':\n"));
-
     } while (validacion != 0);
 }
