ed9ef5e22f6f191b87ea361d58b48a81badfd148
A-Diaz-M <alejandro.diaz36@educa.madrid.org>
2026-01-15 14:10:47 +0100

    a

---
+++ src/b300oobasica/E305RelojHMBasico/Reloj.java
@@ -0,0 +1,28 @@
+package b300oobasica.E305RelojHMBasico;
+
+public class Reloj {
+    public Reloj(int i) {
+    }
+
+    public Reloj(int i, int i1) {
+    }
+
+    public Reloj() {
+
+    }
+
+    public void tick() {
+    }
+
+    public void sumarMinutos(int i) {
+    }
+
+    public void restarMinutos(int i) {
+    }
+
+    public int diferenciaMinutos(Reloj reloj3) {
+    }
+
+    public Reloj diferenciaReloj(Reloj reloj3) {
+    }
+}
