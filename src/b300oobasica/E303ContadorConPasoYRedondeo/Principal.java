1c5de168d17198882cd3855942a566471d17164a
A-Diaz-M <alejandro.diaz36@educa.madrid.org>
2026-01-21 18:38:54 +0100

    a

--- src/b300oobasica/E303ContadorConPasoYRedondeo/Principal.java
+++ src/b300oobasica/E303ContadorConPasoYRedondeo/Principal.java
@@ -5,16 +5,22 @@ public class Principal {
         Contador c1 = new Contador();
         Contador c2 = new Contador(77);
         Contador c3 = new Contador(77, 5);
-        Contador c4 = new Contador(77, 5, false);
+        Contador c4 = new Contador(77, 5, true);
 
+        IO.println("c1");
         System.out.println(c1);
+        IO.println("c2");
         System.out.println(c2);
+        IO.println("c3");
         System.out.println(c3);
+        IO.println("c4");
         System.out.println(c4);
 
+        IO.println("c4");
         c4.incrementar();
         System.out.println(c4);
 
+        IO.println("c4");
         c4.decrementar();
         System.out.println(c4);
     }
