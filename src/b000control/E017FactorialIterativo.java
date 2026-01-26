2dcc41260bfc076c31003deaec7f24ecbec26777
A-Diaz-M <alejandro.diaz36@educa.madrid.org>
2026-01-26 13:14:22 +0100

    Programas

--- src/b000control/E017FactorialIterativo.java
+++ src/b000control/E017FactorialIterativo.java
@@ -3,16 +3,16 @@ void main() {
     String nombre = IO.readln("Dime como te llamas:\n");
 
     int num = Integer.parseInt(IO.readln(nombre + ", introduzca un numero:\n"));
-    int base = 1;
+    int resultado = 1;
 
     int a = Math.abs(num);
 
     for (int i = 1; i <= a; i++) {
-        base = i * base;
+        resultado = i * resultado;
     }
 
     if (num < 0) {
-        base = -base;
+        resultado = -resultado;
     }
-    System.out.format("El numero %d factorial iterativo es igual a: %d\n", num, base);
+    System.out.format("El numero %d factorial iterativo es igual a: %d\n", num, resultado);
 }
