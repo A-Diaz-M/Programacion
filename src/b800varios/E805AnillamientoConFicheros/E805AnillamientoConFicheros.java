cc907d0835080c0834fb4a5aab8f2bd4d86ff317
A-Diaz-M <alejandro.diaz36@educa.madrid.org>
2026-04-20 19:23:12 +0200

    a

---
+++ src/b800varios/E805AnillamientoConFicheros/E805AnillamientoConFicheros.java
@@ -0,0 +1,39 @@
+package b800varios.E805AnillamientoConFicheros;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class E805AnillamientoConFicheros {
+    public static void main(String[] args) {
+        Scanner entrada = null;
+        PrintWriter salida = null;
+
+        try {
+            entrada = new Scanner(new BufferedReader(new FileReader("src/b800varios/E805AnillamientoConFicheros/entrada.txt")));
+            salida = new PrintWriter(new BufferedWriter(new FileWriter("src/b800varios/E805AnillamientoConFicheros/salida.txt")));
+
+            int campanna;
+
+            while (entrada.hasNextInt()) {
+                campanna = entrada.nextInt();
+                if (campanna == 0) break;
+
+                int totales = 0;
+
+                for (int i = 0; i < campanna; i++) {
+                    int capt = entrada.nextInt();
+                    int reg = entrada.nextInt();
+                    totales += (capt - reg);
+                }
+
+                salida.println(totales);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (entrada != null) entrada.close();
+            if (salida != null) salida.close();
+        }
+    }
+}
